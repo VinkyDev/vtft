4579b99dc5bbc6d78bcdd688d6452c428b48da25
Vinky <lwqykst@yeah.net>
2025-12-13 02:48:13 +0800

    chore: 简化埋点

--- apps/electron/src/main/index.ts
+++ apps/electron/src/main/index.ts
@@ -6,7 +6,6 @@ import { ipcInit } from './ipc/index'
 import { createWindow } from './mainWIndow'
 import { createTray } from './tray'
 import { checkForUpdate } from './update'
-import { markReportedToday, shouldReportToday } from './utils/dailyReport'
 
 const aptabaseCode = import.meta.env.VITE_APTABASE_CODE || ''
 
@@ -19,19 +18,12 @@ if (aptabaseCode) {
 
 app.whenReady().then(() => {
   trackEvent('app_start')
-
-  if (shouldReportToday()) {
-    trackEvent('daily_active_user')
-    markReportedToday()
-  }
-
   electronApp.setAppUserModelId('com.vtft.app')
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
 
   ipcInit()
-
   createWindow()
   createTray()
   checkForUpdate()
@@ -40,8 +32,4 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0)
       createWindow()
   })
-})
-
-app.on('will-quit', () => {
-  trackEvent('app_quit')
 })
